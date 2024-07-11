4a2f08a4b329af0f1209a3e679cb431c78272097
JulianPoltro <julianpoltronieri@hotmail.com>
2024-07-11 19:20:25 -0300

    obteniendo prodcuto por ID

--- app.js
+++ app.js
@@ -22,8 +22,16 @@ app.get('/productos', async (req, res) => {
     const productos = await Product.find(query)
     res.json(productos)
   } catch (error) {
-    res.status(500).send('Error al obtener los productos')
+    res.status(500).send('Error al obtener los productos de la lista de precios')
   }
+})
+
+//Obteniendo productos de la lista de precios por ID
+app.get('/productos/:id', async (req, res) => {
+  const { id } = req.params
+  const producto = await Product.findById(id)
+  if (producto) return res.json(producto)
+  res.status(404).json({ message: 'Error al obtener producto por ID de la lista de precios' })
 })
 
 //Agrega un nuevo producto a la lista
