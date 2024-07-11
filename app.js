e97d184e66cdac68fa4c549000bece26f59b0245
JulianPoltro <julianpoltronieri@hotmail.com>
2024-07-11 19:06:18 -0300

    mostrando productos

--- app.js
+++ app.js
@@ -4,16 +4,39 @@ process.loadEnvFile()
 const port = process.env.PORT ?? 3008
 const morgan = require('morgan')
 const connectDB = require('./src/database.js')
+const Products = require('./src/product.js')
 
 app.use(express.json())
 app.use(morgan('dev'))
 connectDB()
 
-
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Bienvenido a la API del SuperMarket!')
 })
 
+//Obtener todos los productos de la lista de precios
+app.get('/productos', async (req, res) => {
+  const { categoria } = req.query
+  const query = !categoria ? {} : { categoria: { $regex: categoria, $options: 'i' } }
+
+  try {
+    const productos = await Products.find(query)
+    res.json(productos)
+  } catch (error) {
+    res.status(500).send('Error al obtener todos los productos de la lista de precios')
+  }
+})
+
+//Agrega un nuevo producto a la lista
+app.post('/productos', async (req, res) => {
+  const nuevoProducto = new Products(req.body)
+  try {
+    await nuevoProducto.save()
+    res.status(201).json(nuevoProducto)
+  } catch {
+    return res.status(500).json({ message: 'Error al agregar el nuevo producto a la lista' })
+  }
+})
 
 
 app.listen(port, () => {
--- src/product.js
+++ src/product.js
@@ -9,6 +9,6 @@ const productSchema = new mongoose.Schema(
 
 })
 
-const Products = mongoose.model('Product', productSchema)
+const Product = mongoose.model('Product', productSchema)
 
-module.exports = Products
+module.exports = Product
